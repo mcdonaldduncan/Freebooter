b369049ea07eaa22901b9b0cc2d711fe909b2d9f
mcdonaldduncan <380939@colum.edu>
2022-09-28 19:35:08 -0500

    Update TurretBehaviour.cs
    change limiting behavior

--- Assets/Scripts/Enemies/Turret Enemy/TurretBehaviour.cs
+++ Assets/Scripts/Enemies/Turret Enemy/TurretBehaviour.cs
@@ -42,6 +42,7 @@ public class TurretBehaviour : MonoBehaviour
     }
     void Aim() //This is pointing the torret towards the player as long as he is in range
     {
+        float tempSpeed = speed;
         if (Vector3.Distance(this.transform.position, target.transform.position) < range)
         { 
             targetDiretion = transform.position - target.transform.position;
@@ -54,24 +55,35 @@ public class TurretBehaviour : MonoBehaviour
             {
                 Vector3 tempRotation = Quaternion.LookRotation(targetDiretion).eulerAngles;
 
-                if (tempRotation.y <= 180 && tempRotation.y >= 0)
-                {
-                    lastValidY = tempRotation.y;
-                }
+                // This would be the best option if we want it to "lose sight" of the player when it gets behind it,
+                // it wouldnt snap to the other side until the player enters their radius on that side.
+                //if (tempRotation.y <= 180 && tempRotation.y >= 0)
+                //{
+                //    lastValidY = tempRotation.y;
+                //}
 
-                if (tempRotation.y < 0)
+
+                // This would be best if the turret always knows where you are, it will snap to the side closest to where the player would reenter their radius.
+                // To make this work with the above commented out code, you would switch the if statements to if (tempRotation.y > 180) { tempRotation.y = lastValidY }
+                if (tempRotation.y > 270 && tempRotation.y > 180)
                 {
-                    tempRotation.y = lastValidY;
+                    tempSpeed *= .1f;
+                    tempRotation.y = 0.5f;
                 }
-                else if (tempRotation.y > 180)
+                if (tempRotation.y <= 270 && tempRotation.y > 180)
                 {
-                    tempRotation.y = lastValidY;
+                    tempSpeed *= .1f;
+                    tempRotation.y = 179.5f;
                 }
 
                 rotation = Quaternion.Euler(tempRotation);
             }
             
-            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, speed * Time.deltaTime);
+            // Lerp can be somewhat low performance because it starts dealing with extremely small increments at the end,
+            // rotateTowards keeps the movement constant in degree/second
+
+            //transform.rotation = Quaternion.Lerp(transform.rotation, rotation, tempSpeed * Time.deltaTime);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, tempSpeed * Time.deltaTime * 180);
         }
        
     }
