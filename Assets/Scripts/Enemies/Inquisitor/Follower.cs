91b3d657117639f1b60a7e92afb47c84a8150eed
mcdonaldduncan <380939@colum.edu>
2022-10-12 23:05:05 -0500

    Add global matchmaking

--- Assets/Scripts/Enemies/Inquisitor/Follower.cs
+++ Assets/Scripts/Enemies/Inquisitor/Follower.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEditor.Progress;
 
 public class Follower : MonoBehaviour
 {
