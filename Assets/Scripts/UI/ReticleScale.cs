3414cbce5e538a1886ce055a8cde9d9d2b50bf5b
TJCampo <82006286+TJCampo@users.noreply.github.com>
2022-10-12 21:27:16 -0500

    Created a new scene and added UI

---
+++ Assets/Scripts/UI/ReticleScale.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
+
+public class ReticleScale : MonoBehaviour
+{
+    private RectTransform Reticle;
+
+    CanvasRenderer reticleRenderer;
+
+    private CanvasGroup reticleCanvas;
+
+    [Range(100f, 150)]
+
+    public float size; 
+
+    void Start()
+    {
+        Reticle = GetComponent<RectTransform>();
+        reticleRenderer = GetComponent<CanvasRenderer>();
+        reticleCanvas = GetComponent<CanvasGroup>();
+
+        reticleCanvas.alpha = 0f;
+        
+    }
+
+    private void Update()
+    {
+        Reticle.sizeDelta = new Vector2(size, size);
+    }
+}
