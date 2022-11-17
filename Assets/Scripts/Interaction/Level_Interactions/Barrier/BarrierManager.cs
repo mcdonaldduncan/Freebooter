bfa7b26838e42c5c8cb3885022e5635773b470f2
mcdonaldduncan <380939@colum.edu>
2022-11-16 19:48:13 -0600

    started door work

---
+++ Assets/Scripts/Interaction/Level_Interactions/Barrier/BarrierManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrierManager : Singleton<BarrierManager>
+{
+    List<Key> m_KeyInventory;
+
+    Dictionary<Barrier, List<Key>> m_KeyMap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Interaction/Level_Interactions/Barrier/Key.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
