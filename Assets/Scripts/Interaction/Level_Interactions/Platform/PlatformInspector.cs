7070ae39760f5dbbdb3b53b60a326b3f8a0719fe
mcdonaldduncan <380939@colum.edu>
2022-12-02 03:54:09 -0600

    update

--- Assets/Scripts/Interaction/Level_Interactions/Platform/PlatformInspector.cs
+++ Assets/Scripts/Interaction/Level_Interactions/Platform/PlatformInspector.cs
@@ -25,7 +25,6 @@ public class PlatformInspector : Editor
             platform.m_Nodes.RemoveAt(platform.m_Nodes.Count - 1);
             DestroyImmediate(temp.gameObject);
         }
-
     }
 }
 #endif
