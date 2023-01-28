3e74144ff780d25f6da764d6964b2692f4a79df8
mcdonaldduncan <380939@colum.edu>
2023-01-28 12:56:18 -0600

    add some comments

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -3,19 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+// AudioManager is a singleton class that manages audio playback in the game.
 public class AudioManager : Singleton<AudioManager>
 {
+    // AudioSource component that plays the audio clips
     [SerializeField] AudioSource m_AudioSource;
+    // Image component that displays a navigation icon
     [SerializeField] Image m_NavImage;
 
+    // A queue that stores audio clips to be played
     Queue<AudioClip> m_ClipQueue;
 
+    // On start, the AudioSource component is assigned to the Player game object's AudioSource component
     private void Start()
     {
         if (m_AudioSource != null) return;
         m_AudioSource = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<AudioSource>();
     }
 
+    // In update, if there are any clips in the queue and the audio source is not currently playing, the next clip in the queue is played
     void Update()
     {
         if (m_ClipQueue == null) return;
@@ -26,33 +32,39 @@ public class AudioManager : Singleton<AudioManager>
         }
     }
 
+    // Method that plays an audio clip immediately
     public void PlayClip(AudioClip clip)
     {
         m_AudioSource.clip = clip;
         m_AudioSource.Play();
     }
 
+    // Method that enables the navigation image and sets its sprite
     public void EnableNavImage(Sprite sprite)
     {
         m_NavImage.sprite = sprite;
         m_NavImage.enabled = true;
     }
 
+    // Method that disables the navigation image
     public void DisableNavImage()
     {
         m_NavImage.enabled = false;
     }
 
+    // Method that adds an audio clip to the queue to be played
     public void QueueSound(AudioClip clip)
     {
         m_ClipQueue.Enqueue(clip);
     }
 
+    // Method that clears the audio clip queue
     public void OverrideQueue()
     {
         m_ClipQueue.Clear();
     }
 
+    // Method that checks if the current audio clip is finished playing
     public bool ClearCheck(AudioClip clip)
     {
         return m_AudioSource.clip == clip && !m_AudioSource.isPlaying;
--- Assets/Scripts/Audio/AudioTrigger.cs
+++ Assets/Scripts/Audio/AudioTrigger.cs
@@ -6,57 +6,61 @@ using UnityEngine.UI;
 
 public class AudioTrigger : MonoBehaviour
 {
-    [SerializeField] GameObject m_Activator;
-    [SerializeField] AudioClip m_Clip;
-    [SerializeField] Sprite m_Sprite;
-    [SerializeField] bool m_ActivateMultiple;
+    [SerializeField] GameObject m_Activator; // reference to the gameobject that activates this audio trigger
+    [SerializeField] AudioClip m_Clip; // reference to the audio clip that will be played
+    [SerializeField] Sprite m_Sprite; // reference to the sprite that will be displayed when the audio clip is played
+    [SerializeField] bool m_ActivateMultiple; // a flag that determines if this audio trigger can be activated multiple times or not
 
-    IActivator m_IActivator;
+    IActivator m_IActivator; // reference to the IActivator component on the activator gameobject
 
-    bool m_IsActive;
+    bool m_IsActive; // flag that determines if this audio trigger is currently active
 
     private void OnEnable()
     {
-        if (m_Activator == null) return;
+        if (m_Activator == null) return; // if there is no activator gameobject, exit the function
 
         try
         {
+            // try to get the IActivator component from the activator gameobject
             m_IActivator = (IActivator)m_Activator.GetComponent(typeof(IActivator));
+            // subscribe to the Activate event
             m_IActivator.Activate += OnActivate;
         }
         catch (System.Exception)
         {
-            Debug.LogError("Valid IActivator Not Found");
+            Debug.LogError("Valid IActivator Not Found"); // if the activator gameobject does not have a valid IActivator component, log an error
         }
 
     }
 
     private void OnDisable()
     {
-        if (m_IActivator == null) return;
+        if (m_IActivator == null) return; // if there is no IActivator component, exit the function
+        // unsubscribe to the Activate event
         m_IActivator.Activate -= OnActivate;
     }
 
     void OnActivate()
     {
-        if (m_IsActive && !m_ActivateMultiple) return;
+        if (m_IsActive && !m_ActivateMultiple) return; // if this audio trigger is currently active and it's not set to activate multiple times, exit the function
 
-        AudioManager.Instance.PlayClip(m_Clip);
-        AudioManager.Instance.EnableNavImage(m_Sprite);
+        AudioManager.Instance.PlayClip(m_Clip); // play the audio clip
+        AudioManager.Instance.EnableNavImage(m_Sprite); // enable the navigation image
 
-        m_IsActive = true;
+        m_IsActive = true; // set the flag to indicate that this audio trigger is currently active
     }
 
     private void Update()
     {
-        if (!m_IsActive) return;
+        if (!m_IsActive) return; // if this audio trigger is not currently active, exit the function
         if (AudioManager.Instance.ClearCheck(m_Clip))
         {
-            AudioManager.Instance.DisableNavImage();
+            AudioManager.Instance.DisableNavImage(); // if the audio clip has finished playing, disable the navigation image
         }
     }
 
 }
+
 
 enum BehaviorType
 {
