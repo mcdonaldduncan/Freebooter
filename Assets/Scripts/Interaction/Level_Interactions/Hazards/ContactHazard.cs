14f6a91d89b97f5dba9ab240c1dfea671b0f0368
Duncan McDonald <72177724+mcdonaldduncan@users.noreply.github.com>
2023-01-30 10:45:16 -0600

    update

---
+++ Assets/Scripts/Interaction/Level_Interactions/Hazards/ContactHazard.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContactHazard : MonoBehaviour
+{
+    [SerializeField] bool instaKill;
+    [SerializeField] bool isLive;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
