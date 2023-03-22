37ef17d9e4da4c00b370bb913558e1e7086b280b
mcdonaldduncan <380939@colum.edu>
2023-03-22 16:44:47 -0500

    updated main menu to include multiple images

--- Assets/Scripts/MainMenu/MainMenuManager.cs
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 /// <summary>
 /// 
@@ -9,6 +10,9 @@ using UnityEngine.SceneManagement;
 /// Author: Duncan McDonald
 public class MainMenuManager : MonoBehaviour
 {
+    [SerializeField] Sprite[] Sprites;
+    [SerializeField] Image Image;
+
     public void StartLevel(int index)
     {
         SceneManager.LoadScene(index);
@@ -20,5 +24,8 @@ public class MainMenuManager : MonoBehaviour
         Application.Quit();
     }
 
-
+    public void SetBackgroundImage(int index)
+    {
+        Image.sprite = Sprites[index];
+    }
 }
