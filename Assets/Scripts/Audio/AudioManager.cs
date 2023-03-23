57800b62b6a082bbeae8782d4a5a84043d3d8641
mcdonaldduncan <380939@colum.edu>
2023-03-23 17:28:32 -0500

    add combat audio system

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -10,13 +10,45 @@ using UnityEngine.UI;
 public class AudioManager : Singleton<AudioManager>
 {
     [SerializeField] AudioSource m_AudioSource;// AudioSource component that plays the audio clips
+    [SerializeField] AudioSource m_PrimaryMusicSource;
+    [SerializeField] AudioSource m_SecondaryMusicSource;
     [SerializeField] Image m_NavImage; // Image component that displays a navigation icon
+    [SerializeField] float m_TransitionSpeed;
+    [SerializeField] float m_ReleaseTime;
+    [SerializeField] float m_InitialCombatVolume;
+
+
+    [SerializeField] AudioClip m_ExplorationMusic;
+    [SerializeField] AudioClip m_CombatMusic;
+    [SerializeField] AudioClip m_ReleaseMusic;
+
+    AudioSource m_CurrentPrimary;
+    AudioSource m_CurrentSecondary;
 
     Queue<AudioClip> m_ClipQueue; // A queue that stores audio clips to be played
+
+    float m_LastReleaseTime;
+
+    bool m_IsReleasing;
+
+    bool m_Shouldrelease => m_IsReleasing && Time.time > m_ReleaseTime + m_LastReleaseTime;
 
     // On start, the AudioSource component is assigned to the Player game object's AudioSource component
     private void Start()
     {
+        LevelManager.Instance.CombatStateChanged += OnCombatStateChanged;
+
+        if (m_PrimaryMusicSource == null || m_SecondaryMusicSource == null)
+        {
+            Debug.LogError("Music sources are not set up correctly");
+        }
+
+        m_PrimaryMusicSource.clip = m_ExplorationMusic;
+        m_PrimaryMusicSource.Play();
+
+        m_CurrentPrimary = m_PrimaryMusicSource;
+        m_CurrentSecondary = m_SecondaryMusicSource;
+
         if (m_AudioSource != null) return;
         m_AudioSource = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<AudioSource>();
     }
@@ -24,6 +56,13 @@ public class AudioManager : Singleton<AudioManager>
     // In update, if there are any clips in the queue and the audio source is not currently playing, the next clip in the queue is played
     void Update()
     {
+        AdjustPrimaryVolume();
+        AdjustSecondaryVolume();
+
+        if (m_Shouldrelease) ExplorationTransition();
+        
+
+
         if (m_ClipQueue == null) return;
         if (!m_AudioSource.isPlaying && m_ClipQueue.Count > 0)
         {
@@ -68,5 +107,53 @@ public class AudioManager : Singleton<AudioManager>
     public bool ClearCheck(AudioClip clip)
     {
         return m_AudioSource.clip == clip && !m_AudioSource.isPlaying;
+    }
+
+    private void OnCombatStateChanged(bool inCombat)
+    {
+        var temp = m_CurrentPrimary;
+
+        m_CurrentPrimary = m_CurrentSecondary;
+        m_CurrentPrimary.clip = inCombat ? m_CombatMusic : m_ReleaseMusic;
+        m_CurrentPrimary.Play();
+
+        m_CurrentSecondary = temp;
+        
+        
+
+        if (!inCombat)
+        {
+            m_IsReleasing = true;
+            m_LastReleaseTime = Time.time;
+        }
+        else
+        {
+            m_CurrentPrimary.volume = m_InitialCombatVolume;
+        }
+    }
+
+    private void ExplorationTransition()
+    {
+        var temp = m_CurrentPrimary;
+
+        m_CurrentPrimary = m_CurrentSecondary;
+        m_CurrentPrimary.clip = m_ExplorationMusic;
+        m_CurrentPrimary.Play();
+
+        m_CurrentSecondary = temp;
+
+        m_IsReleasing = false;
+    }
+
+    public void AdjustPrimaryVolume()
+    {
+        if (m_CurrentPrimary.volume == 1) return;
+        m_CurrentPrimary.volume = Mathf.MoveTowards(m_CurrentPrimary.volume, 1, m_TransitionSpeed * Time.deltaTime);
+    }
+
+    public void AdjustSecondaryVolume()
+    {
+        if (m_CurrentPrimary.volume == 0) return;
+        m_CurrentSecondary.volume = Mathf.MoveTowards(m_CurrentSecondary.volume, 0, m_TransitionSpeed * Time.deltaTime);
     }
 }
