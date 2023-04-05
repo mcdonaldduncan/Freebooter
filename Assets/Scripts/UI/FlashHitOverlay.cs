e068f571eedeb13a4626b5324b03581bfc27726a
Sebastian Pedersen <57201896+Sebp23@users.noreply.github.com>
2023-04-05 09:15:39 -0500

    Fixed a bug that caused the player hit splatter to play when the player healed

--- Assets/Scripts/UI/FlashHitOverlay.cs
+++ Assets/Scripts/UI/FlashHitOverlay.cs
@@ -9,6 +9,8 @@ public class FlashHitOverlay : MonoBehaviour
     [SerializeField] private float m_fadeTime;
     
     private float m_alpha;
+    private float m_playerHealthBefore;
+    private float m_playerHealthAfter;
 
     private Image m_overlayImage;
     private Color m_overlayColorHit;
@@ -19,6 +21,7 @@ public class FlashHitOverlay : MonoBehaviour
         m_overlayImage = gameObject.GetComponent<Image>();
 
         LevelManager.Instance.Player.PlayerHealthChanged += OnPlayerHit;
+        m_playerHealthBefore = LevelManager.Instance.Player.Health;
         m_overlayColorFaded = new Color(m_overlayImage.color.r, m_overlayImage.color.g, m_overlayImage.color.b, 0);
         m_overlayColorHit = new Color(m_overlayImage.color.r, m_overlayImage.color.g, m_overlayImage.color.b, 1);
 
@@ -40,7 +43,12 @@ public class FlashHitOverlay : MonoBehaviour
 
     private void OnPlayerHit()
     {
-        m_alpha = 1f;
-        m_overlayImage.color = m_overlayColorHit;
+        m_playerHealthAfter = LevelManager.Instance.Player.Health;
+        if (m_playerHealthAfter < m_playerHealthBefore)
+        {
+            m_alpha = 1f;
+            m_overlayImage.color = m_overlayColorHit;
+        }
+        m_playerHealthBefore = m_playerHealthAfter;
     }
 }
