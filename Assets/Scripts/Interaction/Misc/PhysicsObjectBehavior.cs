cf04ab3d82b117ef6dfa70d3731d91a280b7489b
Sebastian Pedersen <57201896+Sebp23@users.noreply.github.com>
2023-04-26 12:41:42 -0500

    Added force behavior to physics props (flasks, books, etc.)

---
+++ Assets/Scripts/Interaction/Misc/PhysicsObjectBehavior.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsObjectBehavior : MonoBehaviour, IDamageable
+{
+    #region UnusedIDamageableStuff
+    public float Health { get; set; }
+
+    public GameObject DamageTextPrefab { get; }
+
+    public Transform TextSpawnLocation { get; }
+
+    public float FontSize { get; }
+
+    public void CheckForDeath()
+    {
+        throw new System.NotImplementedException();
+    }
+    #endregion
+
+    [SerializeField] private float m_force;
+    [SerializeField] private float m_forceScale;
+
+    private Rigidbody m_objRB;
+
+    private void Start()
+    {
+        m_objRB = GetComponent<Rigidbody>();
+    }
+
+    public void TakeDamage(float damageTaken, HitBoxType hitbox, Vector3 hitPoint = default)
+    {
+        Vector3 forceDirection = m_objRB.position - hitPoint;
+        m_objRB.AddForce(forceDirection.normalized * m_force * m_forceScale, ForceMode.Impulse);
+    }
+}
