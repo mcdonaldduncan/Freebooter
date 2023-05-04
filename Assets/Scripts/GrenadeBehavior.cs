12099a85ffa9decb903d9931d93b907d9cce510f
Sebastian Pedersen <57201896+Sebp23@users.noreply.github.com>
2023-05-04 12:43:34 -0500

    Removed PackageManager import in GrenadeBehavior.cs
    Should fix build error

--- Assets/Scripts/GrenadeBehavior.cs
+++ Assets/Scripts/GrenadeBehavior.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class GrenadeBehavior : MonoBehaviour, IPoolable, IDamageTracking
