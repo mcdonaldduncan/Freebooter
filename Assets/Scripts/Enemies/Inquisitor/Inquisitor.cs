5ad2c13da380183ce93c9b3aa3b2d8057c614479
Sebastian Pedersen <57201896+Sebp23@users.noreply.github.com>
2023-05-10 10:32:17 -0500

    Fixed Inquisitor Tilting Issue

--- Assets/Scripts/Enemies/Inquisitor/Inquisitor.cs
+++ Assets/Scripts/Enemies/Inquisitor/Inquisitor.cs
@@ -267,7 +267,7 @@ public class Inquisitor : MonoBehaviour, IDamageable, IGroupable
             normalCross = Vector3.zero;
         }
         Vector3 adjustedLook = (m_Target.position + normalCross * 30f) - transform.position;
-        var rotGoal = Quaternion.LookRotation(new Vector3(adjustedLook.x, transform.position.y, adjustedLook.z));
+        var rotGoal = Quaternion.LookRotation(new Vector3(adjustedLook.x, /*transform.position.y*/0, adjustedLook.z));
         transform.rotation = Quaternion.RotateTowards(transform.rotation, rotGoal, m_RotSpeed * Time.deltaTime);
         
         m_AttackSpawn.transform.LookAt(m_TargetPosition);
